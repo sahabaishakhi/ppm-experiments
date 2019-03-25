e167dd6cdf5d43dd831863dcda89b67d05731df2
sahabaishakhi <baishakhi@front.live>
2019-03-25 20:26:51 +0530

    new flow commit

--- src/ppm_1_threading/baishakhi.clj
+++ src/ppm_1_threading/baishakhi.clj
@@ -5,42 +5,41 @@
 (def head (str "P3\n" 255 " " 255 " " 255 "\n"))
 
 (defn red []
-  (map mod (range (* size size)) (repeat size))
-                                        ;(->> (repeat size) map mod (range (* size size)))  
-  )
+  (map mod (range (* size size)) (repeat size)))
 
 (defn green []
-  (flatten (map repeat (repeat size) (range size)))
-                                        ; (->> (range size) (map repeat (repeat size)) flatten)
-  )
+  (flatten (map repeat (repeat size) (range size))))
 
 (defn blue []
   (map mod (map * (red) (green)) (repeat size)))
 
-                                        ;testing
+;testing
 (red)
+(type (red))
 (count (red))
+
 (green)
+(type (green))
 (count (green))
+
 (blue)
+(type (blue)) 
 (count (blue))
 
 (defn image-data-vals []
   (map str (red) (repeat " ") (green) (repeat " ") (blue)))
 
 (image-data-vals)
-(type (list image-data-vals))
+(type (image-data-vals))
 (count (image-data-vals))
 
 (defn image-data []
-  (clojure.string/join " " (list image-data-vals)))
+  (clojure.string/join " " (image-data-vals)))
 
 (image-data)
 
-(defn ppm-content [] (str head "\n" image-data))
+(defn ppm-content [] (str head "\n" (image-data)))
 
 (ppm-content)
 
-(spit "threadppmdemo.ppm" ppm-content)
-
-                                        ;(spit "threadppmdemo.ppm" (str head "\n" "0 0 255 255 0 0 0 255 0 255 0 0"))
+(spit "threadppmdemo.ppm" (ppm-content))
